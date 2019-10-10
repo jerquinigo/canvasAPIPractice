c9ef86811f6ad9343653325d1e69fbb34678e252
Jonathan.e <jonathanerquinigo@pursuit.org>
2019-10-10 12:37:34 -0400

    working with fillstyle and fillrect

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
     let canvas = document.getElementById("mainCanvas")
     let ctx = canvas.getContext("2d")
+
+    ctx.fillStyle = 'rgb(200, 0, 0)';
+    //fill rect (x,y,width,height)
+    ctx.fillRect(20, 30, 100, 100);
+
+    ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
+        ctx.fillRect(40, 50, 100, 100);
 })
